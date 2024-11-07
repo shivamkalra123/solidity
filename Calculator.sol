ba605c21592c32ca06d8b23c4597cddb2f26235d
Shivam Kalra <60731601+shivamkalra123@users.noreply.github.com>
2024-11-07 21:59:42 +0530

    Add files via upload

---
+++ Calculator.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Calculator {
+    uint256 result = 0;
+    function add(uint256 num) public {
+        result += num;
+    }
+    function subtract(uint256 num) public {
+        result -= num;
+    }
+   
+    function get() public view returns (uint256) {
+        return result;
+    }
+}
